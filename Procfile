51945b9686967216b2f90b451de65eb600567107
Logesh Aravind <logaravind1998@gmail.com>
2023-10-11 10:14:01 +0530

    file structure added

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn -k eventlet -w 1 app:app --log-file=-
+postdeploy: sh setup.sh
---
+++ setup.sh
@@ -0,0 +1,3 @@
+# setup.sh
+apt-get update
+apt-get install -y libsm6 libxrender1 libfontconfig1 libice6 libgl1 libgl1-mesa-glx libglu1 libgl1-mesa-dev
